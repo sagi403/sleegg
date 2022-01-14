5d0c73039bd879edd09c1ef4568d4a02f88fcad9
Sagi <sagi403@gmail.com>
2022-01-14 13:58:05 +0200

    Cart reducer & add to cart action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ function App() {
             <Route path="/" element={<HomeScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart/:id" element={<CartScreen />} />
+            <Route path="/cart" element={<CartScreen />} />
           </Routes>
         </Container>
       </main>
