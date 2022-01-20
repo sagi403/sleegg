eb14cee611ddbdb525a19fda0d99d97b665685b5
Sagi <sagi403@gmail.com>
2022-01-20 09:39:27 +0200

    Admin screen access security

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -17,6 +17,7 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
+  USER_LIST_RESET,
 } from "../constants/userConstants";
 import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
 
@@ -57,9 +58,14 @@ export const login = (email, password) => async dispatch => {
 
 export const logout = () => dispatch => {
   localStorage.removeItem("userInfo");
+  localStorage.removeItem("cartItems");
+  localStorage.removeItem("shippingAddress");
+  localStorage.removeItem("paymentMethod");
   dispatch({ type: USER_LOGOUT });
   dispatch({ type: USER_DETAILS_RESET });
   dispatch({ type: ORDER_LIST_MY_RESET });
+  dispatch({ type: USER_LIST_RESET });
+  document.location.href = "/login";
 };
 
 export const register = (name, email, password) => async dispatch => {
