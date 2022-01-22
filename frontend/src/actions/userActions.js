e03a1f1246d1ccd13674007a39cbe62da6100bc3
Sagi <sagi403@gmail.com>
2022-01-22 11:42:34 +0200

    Front end product reviews

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -26,6 +26,7 @@ import {
   USER_UPDATE_FAIL,
 } from "../constants/userConstants";
 import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
+import { PRODUCT_CREATE_REVIEW_RESET } from "../constants/productConstants";
 
 export const login = (email, password) => async dispatch => {
   try {
@@ -71,6 +72,7 @@ export const logout = () => dispatch => {
   dispatch({ type: USER_DETAILS_RESET });
   dispatch({ type: ORDER_LIST_MY_RESET });
   dispatch({ type: USER_LIST_RESET });
+  dispatch({ type: PRODUCT_CREATE_REVIEW_RESET });
   document.location.href = "/login";
 };
 
