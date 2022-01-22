1c285dd2e72847f41ace4e771fe3f5dd53961b88
Sagi <sagi403@gmail.com>
2022-01-22 14:01:48 +0200

    Product pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,12 +39,21 @@ function App() {
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
             <Route path="/admin/productlist" element={<ProductListScreen />} />
             <Route
+              path="/admin/productlist/:pageNumber"
+              element={<ProductListScreen />}
+            />
+            <Route
               path="/admin/product/:id/edit"
               element={<ProductEditScreen />}
             />
             <Route path="/admin/orderlist" element={<OrderListScreen />} />
             <Route path="/cart" element={<CartScreen />} />
             <Route path="/search/:keyword" element={<HomeScreen />} />
+            <Route path="/page/:pageNumber" element={<HomeScreen />} />
+            <Route
+              path="/search/:keyword/page/:pageNumber"
+              element={<HomeScreen />}
+            />
             <Route path="/" element={<HomeScreen />} />
           </Routes>
         </Container>
